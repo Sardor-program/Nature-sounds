6c2d66e34b2456aeb2282fb6d22f888af69d5fea
Sardor-program <137090102+Sardor-program@users.noreply.github.com>
2023-10-16 16:46:15 +0500

    nature

---
+++ script.js
@@ -0,0 +1,79 @@
+let play = document.getElementById("play");
+let stop = document.getElementById("stop");
+let range = document.getElementById("range");
+
+let audio1 = new Audio("sounds/rain-sounds-relaxing-noise-and-sound-of-summer-rain-143334.mp3");
+let audio2 = new Audio('sounds/natural-thunder-113219.mp3')
+let audio3 = new Audio('sounds/forest-ambience-with-cuckoo-birds-chirping-123046.mp3')
+let audio4 = new Audio('sounds/wind-128967.mp3')
+let audio5 = new Audio('sounds/sound-of-a-quiet-spring-stream-background-sounds-of-nature-144993.mp3')
+let audio6 = new Audio('sounds/ocean-sea-soft-waves-121349.mp3')
+
+play.addEventListener('click', ()=>{
+    audio1.play()
+    
+})
+stop.addEventListener('click', ()=>{
+    audio1.pause()
+})
+
+
+play1.addEventListener('click', ()=>{
+    audio2.play()
+    
+})
+stop1.addEventListener('click', ()=>{
+    audio2.pause()
+})
+
+
+
+play2.addEventListener('click', ()=>{
+    audio3.play()
+    
+})
+stop2.addEventListener('click', ()=>{
+    audio3.pause()
+})
+
+
+
+play3.addEventListener('click', ()=>{
+    audio4.play()
+    
+})
+stop3.addEventListener('click', ()=>{
+    audio4.pause()
+})
+
+
+
+play4.addEventListener('click', ()=>{
+    audio5.play()
+    
+})
+stop4.addEventListener('click', ()=>{
+    audio5.pause()
+})
+
+
+
+play5.addEventListener('click', ()=>{
+    audio6.play()
+    
+})
+stop5.addEventListener('click', ()=>{
+    audio6.pause()
+})
+
+
+
+
+
+    
+
+
+
+
+
+    
